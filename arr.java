d2a8a6c2c28e77ff304b494217e57f2c94afbe59
Akil Shaikh <96439306+Akilshaik@users.noreply.github.com>
2022-10-27 15:01:42 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,21 @@
+package non_premitive;
+
+public class arr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]=new int[4];
+arr[0]=1;
+arr[1]=2;
+arr[2]=4;
+arr[3]=9;
+System.out.println("Arry element are");
+System.out.println(arr[0]);
+System.out.println(arr[1]);
+System.out.println(arr[2]);
+System.out.println(arr[3]);
+int A = arr[0]+ arr[1] + arr[2] + arr[3];
+ System.out.println("Addition of arry is ="+A);
+	}
+
+}
