71a7d4f19dfffce2fa72f8749b3240c1b09a1bd3
Akil Shaikh <96439306+Akilshaik@users.noreply.github.com>
2022-11-15 10:59:23 +0530

    Add files via upload

---
+++ boolen.java
@@ -0,0 +1,47 @@
+package javaa;
+
+public class boolen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//boolean example
+		boolean IoT=true;
+		System.out.println(IoT);
+		//byte example
+		byte range;
+		range=127;
+		System.out.println(range);
+		
+		// int exmaple
+		int range2;
+		range2=300000000;
+		System.out.println(range2);
+		
+		// long example
+		long range3;
+		range3=3000000000L;
+		System.out.println(range3);
+		
+		// 	double example
+		double number=99.9;
+		System.out.println(number);
+		
+		//float example
+		float number1=99.9f;
+		System.out.println(number1);
+		
+		// char example 
+		char later='\u0041';
+		System.out.println(later);
+		
+		char later2=65;
+		System.out.println(later2);
+		
+		char later3='A';
+		System.out.println(later3);
+		
+	}
+
+
+
+}
