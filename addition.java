d366fd295e4ebfa7c34b963f5fb6920f4852da64
Akil Shaikh <96439306+Akilshaik@users.noreply.github.com>
2022-11-20 17:16:06 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,13 @@
+package javaa;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20, b=130, c=0 ;
+		c=a+b;
+		System.out.println("Addition of a and b = "+c);
+
+	}
+
+}
